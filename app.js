04fcba57fe3639b894f1d360aab54e777cff85ca
hyukjung <hyukjung1013@gmail.com>
2019-05-06 15:08:08 +0900

    2nd Commit: (1) cookie.js added. (for parsing req.headers.cookie) (2) cookie option.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var http = require('http');
+var cookie = require('cookie');
 
 http.createServer((req, res) => {
+
+    console.log(req.headers.cookie);
+    // "name=ronaldo; nation=portugal"
+
+    var cookies = {};
+    if(req.headers.cookie !== undefined) {
+        cookies = cookie.parse(req.headers.cookie);
+    }
+
     res.writeHead(300, {
         'Set-Cookie': [
             'name=ronaldo',
-            'nation=portugal'
+            'nation=portugal',
+            `permenent=permenent; Max-Age=${60*60*24*30}`,
+            'secure=secure; Secure',
+            'httpOnly=httpOnly; HttpOnly',
+            'path=path; Path=/cookie',
+            'domain=domain; Domain=myApp.test.com'
         ]
     });
     res.end('Hello world!');
